8ad6396b37a08f9cb9731d7ab39997f84b832ae9
Vlad Diachenko <dev.vdiachenko@gmail.com>
2018-08-25 00:24:40 +0300

    :hammer: Refactoring

--- src/games/balance.js
+++ src/games/balance.js
@@ -4,21 +4,26 @@ import gameEngine from '../game-engine';
 const sortByAsc = arr => arr.sort((a, b) => +a - +b);
 
 const getBalancedNumber = (num) => {
+  let result;
+
   const arr = num.toString().split('');
-  const sortedArr = sortByAsc(arr);
-  const minIndex = 0;
-  const maxIndex = sortedArr.length - 1;
-  const min = +sortedArr[minIndex];
-  const max = +sortedArr[maxIndex];
-  const diff = max - min;
-  if (diff <= 1) {
-    return sortedArr.join('');
+
+  while (!result) {
+    const sortedArr = sortByAsc(arr);
+    const minIndex = 0;
+    const maxIndex = sortedArr.length - 1;
+    const min = +sortedArr[minIndex];
+    const max = +sortedArr[maxIndex];
+
+    if (max - min <= 1) {
+      result = sortByAsc(sortedArr).join('');
+    }
+
+    sortedArr[minIndex] = +min + 1;
+    sortedArr[maxIndex] = +max - 1;
   }
 
-  sortedArr[minIndex] = +min + 1;
-  sortedArr[maxIndex] = +max - 1;
-
-  return getBalancedNumber(sortedArr.join(''));
+  return result;
 };
 const description = 'Balance the given number.';
 

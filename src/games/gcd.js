bd8b3aa29db4f210c47abb587bcce31a6de6b534
Vlad Diachenko <dev.vdiachenko@gmail.com>
2018-08-24 10:14:18 +0300

    :sparkles: Implement "Find the greatest common divisor of given numbers." game

---
+++ src/games/gcd.js
@@ -0,0 +1,35 @@
+import random from 'random';
+import GameEngine from '../game-engine';
+
+const getGCD = (a, b) => {
+  if (a === 0) {
+    return b;
+  }
+
+  if (b === 0) {
+    return a;
+  }
+
+  if (a !== b) {
+    return a > b ? getGCD(a - b, b) : getGCD(a, b - a);
+  }
+
+  return a;
+};
+const config = {
+  description: 'Find the greatest common divisor of given numbers.',
+};
+
+const game = () => {
+  const num1 = random.int(0, 10);
+  const num2 = random.int(0, 10);
+  const question = `${num1} ${num2}`;
+  const answer = getGCD(num1, num2);
+
+  return {
+    question,
+    answer,
+  };
+};
+
+export default () => GameEngine(game, config);

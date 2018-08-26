f7c87fa9f9ac1f4e7d3c1af06fcf0d8c188bb50d
Vlad Diachenko <dev.vdiachenko@gmail.com>
2018-08-26 11:15:22 +0300

    :hammer: Fix issues

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,13 +2,13 @@ import random from 'random';
 import gameEngine from '../game-engine';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
-    }
-
-    if (i > Math.sqrt(num)) {
-      return true;
     }
   }
 
